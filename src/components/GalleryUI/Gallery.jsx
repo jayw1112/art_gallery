699db671f23dd76c5807e1f5a50de2b0acf3ae4b
jayw1112 <housexpresso@gmail.com>
2023-05-21 05:34:20 -0700

    Gallery fetches images from featured collection

--- src/components/GalleryUI/Gallery.jsx
+++ src/components/GalleryUI/Gallery.jsx
@@ -2,11 +2,12 @@ import React, { useState, useEffect } from 'react'
 import ImageCard from './ImageCard'
 import classes from './Gallery.module.css'
 import ErrorBoundary from '../Error/ErrorBoundary'
-import { storage, storageRef } from '../../firebase'
+import { db, storage, storageRef } from '../../firebase'
 import { ref, listAll, getDownloadURL, getMetadata } from 'firebase/storage'
 import Spinner from '../UI/Spinner'
 import { getAuth, onAuthStateChanged } from 'firebase/auth'
 import useAuthState from '../../hooks/use-authState'
+import { collection, getDocs } from 'firebase/firestore'
 
 function Gallery() {
   const [images, setImages] = useState([])
@@ -19,40 +20,76 @@ function Gallery() {
     getImages()
   }, [])
 
-  // Second useEffect hook - Handling user's auth state
-  // useEffect(() => {
-  //   const auth = getAuth()
-  //   const unsubscribe = onAuthStateChanged(auth, (user) => {
-  //     if (user) {
-  //       // User is signed in, update the displayName state
-  //       setDisplayName(user.displayName || user.email)
-  //     } else {
-  //       // User is signed out
-  //       setDisplayName('')
-  //     }
-  //   })
-  //   // Cleanup the listener on unmount
-  //   return () => unsubscribe()
-  // }, [])
+  // Fetch selected images from featuredUsers collection
 
   const getImages = async () => {
     setLoading(true)
 
-    const imageUrls = []
-    const imageRef = ref(storage, 'default_pics')
-    const imageNames = await listAll(imageRef)
+    try {
+      const imageDetails = []
+      const featuredUsersSnapshot = await getDocs(
+        collection(db, 'featuredUsers')
+      )
 
-    const fetchUrls = imageNames.items.map(async (itemRef) => {
-      const url = await getDownloadURL(itemRef)
-      // const metadata = await getMetadata(itemRef)
-      // const imageId = metadata.name
-      imageUrls.push(url)
-    })
+      for (const doc of featuredUsersSnapshot.docs) {
+        const { images: featuredImages } = doc.data()
+        const userId = doc.id
+        const imageRef = ref(storage, `users/${userId}`)
 
-    await Promise.all(fetchUrls)
-    setImages(imageUrls)
-    setLoading(false)
+        const fetchImageDetails = featuredImages.map(async (imageName) => {
+          try {
+            const imgRef = ref(imageRef, imageName)
+            const [url, metadata] = await Promise.allSettled([
+              getDownloadURL(imgRef),
+              getMetadata(imgRef),
+            ])
+
+            // Check if both promises fulfilled
+            if (url.status === 'fulfilled' && metadata.status === 'fulfilled') {
+              return { url: url.value, metadata: metadata.value, imageName }
+            } else {
+              console.error(`Failed to fetch details for image ${imageName}`)
+            }
+          } catch (error) {
+            console.error(
+              `Failed to fetch details for image ${imageName}: ${error}`
+            )
+          }
+        })
+
+        const imageDetailsForUser = (
+          await Promise.allSettled(fetchImageDetails)
+        )
+          .filter((result) => result.status === 'fulfilled')
+          .map((result) => result.value)
+
+        imageDetails.push(...imageDetailsForUser)
+      }
+
+      setImages(imageDetails)
+      setLoading(false)
+    } catch (error) {
+      console.error('Error fetching image details:', error)
+      setLoading(false)
+    }
   }
+
+  // const getImages = async () => {
+  //   setLoading(true)
+
+  //   const imageUrls = []
+  //   const imageRef = ref(storage, 'default_pics')
+  //   const imageNames = await listAll(imageRef)
+
+  //   const fetchUrls = imageNames.items.map(async (itemRef) => {
+  //     const url = await getDownloadURL(itemRef)
+  //     imageUrls.push(url)
+  //   })
+
+  //   await Promise.all(fetchUrls)
+  //   setImages(imageUrls)
+  //   setLoading(false)
+  // }
 
   return (
     <>
@@ -66,11 +103,15 @@ function Gallery() {
           images.map((image) => (
             <ErrorBoundary
               fallback={<p>Error Displaying Image.</p>}
-              key={image}
+              key={image.url} // Updated this line to use image.url as the key
             >
               <ImageCard
-                image={image}
-                // imageId={imageId}
+                image={image.url} // Fetching the image URL from the image object
+                title={image.metadata.customMetadata?.title}
+                description={image.metadata.customMetadata?.description}
+                owner={image.metadata.customMetadata?.owner}
+                displayLink={true}
+                imageId={image.imageName}
               />
             </ErrorBoundary>
           ))
